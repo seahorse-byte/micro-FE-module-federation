5a44afd34cb89a03f903744e9e1b9c088fae7de3
Olsi Gjeci <olsi.developer@gmail.com>
2023-09-15 23:15:37 +0100

    micro frontends - module federation with vite

---
+++ host/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './App.css';
+
+import Button from 'remoteApp/Button';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Host application</h1>
+      <Button />
+      <div className="card">
+        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
